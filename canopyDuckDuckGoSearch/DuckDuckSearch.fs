0f9e02afb6b8f086bcbec7d7e05582183c2c1271
Oleg <oleg.zarevych@gmail.com>
2020-01-21 02:08:22 +0200

    add reporting

--- canopyDuckDuckGoSearch/DuckDuckSearch.fs
+++ canopyDuckDuckGoSearch/DuckDuckSearch.fs
@@ -2,19 +2,33 @@
 
 open canopy.runner.classic
 open canopy.classic
+open canopy.reporters
+open canopy.configuration
+open canopy.types
 
 let allTests () = 
 
 before(fun _ -> 
-     canopy.configuration.chromeDir <- System.AppContext.BaseDirectory
+     chromeDir <- System.AppContext.BaseDirectory
      start chrome
      url "https://duckduckgo.com/"
      ()
 )
 
 after(fun _ ->
-    quit chrome
+    //TODO: finish reporting
+    let pathToReports = __SOURCE_DIRECTORY__ + @"\canopy\"
+    reporter <- new ConsoleReporter() :> IReporter
+    //reporter <- new LiveHtmlReporter(chrome, chromeDir) :> IReporter
+    //reporter.reportPath <- Some pathToReports
     ()
+)
+
+onFail (fun _ -> 
+        let pathToScreens = __SOURCE_DIRECTORY__ + @"\canopy\"
+        let filename = System.DateTime.Now.ToString("MMM-d_HH-mm-ss-fff")                
+        screenshot pathToScreens filename
+        |> ignore        
 )
 
 "duckduck search " &&& fun _ ->
--- canopyDuckDuckGoSearch/Program.fs
+++ canopyDuckDuckGoSearch/Program.fs
@@ -1,13 +1,18 @@
 module Program =
 
+open canopy.classic
 open canopy.runner.classic
 open System
+open canopy.reporters
+open canopy.configuration
+open canopy.types
 
     [<EntryPoint>]
     let main _ = 
     DuckDuckSearch.allTests()
     
     run ()
+    quit 
 
     Console.ReadKey() |> ignore
     0
